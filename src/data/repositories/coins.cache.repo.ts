df829a56c310334614a8787060d4451cc2f07a16
websterhf18 <websterh18@gmail.com>
2021-07-31 17:22:29 -0500

    Cache info

--- src/data/repositories/coins.cache.repo.ts
+++ src/data/repositories/coins.cache.repo.ts
@@ -12,6 +12,35 @@ import { CoinsEntity } from "@entity/coins.entity";
 export class CoinsCacheRepo implements CoinsContract {
 
     async getCoins(): Promise<CoinsEntity[]> {
-        throw new Error("Method not implemented.");
+        return [
+            {
+                "symbol": "BTC",
+                "name": "Bitcoin",
+                "nameid": "bitcoin",
+                "rank": 1,
+                "price_usd": "41765.09",
+            },
+            {
+                "symbol": "BTC",
+                "name": "Bitcoin-1",
+                "nameid": "bitcoin",
+                "rank": 1,
+                "price_usd": "41765.09",
+            },
+            {
+                "symbol": "BTC",
+                "name": "Bitcoin-2",
+                "nameid": "bitcoin",
+                "rank": 1,
+                "price_usd": "41765.09",
+            },
+            {
+                "symbol": "BTC",
+                "name": "Bitcoin-3",
+                "nameid": "bitcoin",
+                "rank": 1,
+                "price_usd": "41765.09",
+            },
+        ]
     }
 }
